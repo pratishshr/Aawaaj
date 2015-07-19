d1772d3d1b5bc3e5ffa3b285909c2eab098375fe
vanroshr <vanroshr@gmail.com>
2015-07-19 21:39:43 +0545

    removed map section

--- index.php
+++ index.php
@@ -87,9 +87,6 @@
         </div>
     </section>
 
-    <!-- Map Section -->
-    <div id="map"></div>
-
     <!-- Footer -->
     <footer>
         <div class="container text-center">
@@ -106,9 +103,7 @@
     <!-- Plugin JavaScript -->
     <script src="js/jquery.easing.min.js"></script>
 
-    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
-
+    
     <!-- Custom Theme JavaScript -->
     <script src="js/grayscale.js"></script>
 
