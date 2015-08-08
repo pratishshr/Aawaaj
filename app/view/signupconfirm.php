fbdcbef887d7b1b60e311c29bef41991b993852e
Romit Amgai <rom_amgai@hotmail.com>
2015-08-09 02:59:09 +0545

    Revert "Revert "Revert "minor changes"""
    This reverts commit 9831703ed2852ffb7a7a40eb09c1b58bc31143bd.

--- app/view/signupconfirm.php
+++ app/view/signupconfirm.php
@@ -11,7 +11,6 @@
   <div class="form-group">	
     <input type="text" class="form-control" id="confirmation_code" placeholder="Confirmation Code" name="code">
   </div>
-  <div id="empty"></div>
   <div class="text-center"><button type="submit" class="btn btn-lg btn-default">Submit</button></div>
 </form>
 </div>
