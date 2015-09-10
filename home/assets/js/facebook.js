1f85a6ec3e4aecc637b734a99816babc0afbec99
pratishshr <vanroshr@gmail.com>
2015-09-10 09:38:15 +0545

    added facebook comments

---
+++ home/assets/js/facebook.js
@@ -0,0 +1,15 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '306604469463154',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=306604469463154";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
